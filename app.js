2c6569e03f78ecc544851a657ff8ad64601f5971
Dhruvsingh121 <GodZeusOP@gmail.com>
2025-04-12 11:04:59 +0530

    Create [Fixed] & Added Update Route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 main()
   .then((res) => {
@@ -17,6 +18,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("Root FOlder");
@@ -41,24 +43,26 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create Route
-app.post("listings", async (req, res) => {
-  const newListing = new Listing(req.body.Listing);
+app.post("/listings", async (req, res) => {
+  console.log(req.body); // Debugging: Check the form data
+  const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
 });
 
-// app.get("/test", async (req, res) => {
-//   let sampleListing = new Listing({
-//     title: "My New Villa",
-//     description: "By The Farm",
-//     price: 1200,
-//     location: "Calangute, Goa",
-//     Country: "India",
-//   });
-//   await sampleListing.save();
-//   console.log("Sample Saved !!");
-//   res.send("sucessfull");
-// });
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
 
 app.listen(8080, () => {
   console.log("Server Started!");
